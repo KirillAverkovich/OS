ea7f424cc577dd44713c3d040e199d307dba2300
KirillAverkovich <151824328+KirillAverkovich@users.noreply.github.com>
2024-02-12 21:35:03 +0300

    Create Reporter.cpp

---
+++ laba1/Reporter/Reporter.cpp
@@ -0,0 +1,12 @@
+#include "Reporter.h"
+#pragma warning(disable:4996)
+
+int main(int argc, char* argv[]) 
+{
+	char binfile_name[50];
+	char report_name[50];
+	strcpy(binfile_name, argv[1]);
+	strcpy(report_name, argv[2]);
+	double amount = std::atof(argv[3]);
+	Read_from_bin_file(binfile_name, report_name, amount);
+}
